d1f3b9cc012b4e7d4ede3c46e56ec2deb84e87cd
Mathew Poovathany <poovathanymathew@gmail.com>
2020-08-20 18:35:25 -0400

    Added Heroku DB
    Added Heroku server

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,22 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
+/*
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Kate2016",
     database: "burgers_db"
+  });
+}
+*/
+
+  connection = mysql.createConnection({
+    host: "	a5s42n4idx9husyc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "do8bsashedj7y1op",
+    password: "opzq4qm64fn6z2fj",
+    database: "g0gmnrt8qqfxerf8"
   });
 }
 
