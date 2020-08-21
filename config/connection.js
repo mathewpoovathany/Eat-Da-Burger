e72adc132987aa94dc36f6f3c4be9744a572a94c
Mathew Poovathany <poovathanymathew@gmail.com>
2020-08-21 13:16:20 -0400

    Huroku SQUL server connected
    working model

--- config/connection.js
+++ config/connection.js
@@ -7,19 +7,9 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-/*
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Kate2016",
-    database: "burgers_db"
-  });
-}
-*/
 
   connection = mysql.createConnection({
-    host: "	a5s42n4idx9husyc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
+    host: "a5s42n4idx9husyc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "do8bsashedj7y1op",
     password: "opzq4qm64fn6z2fj",
     database: "g0gmnrt8qqfxerf8"
